c341c0c00aea55f6dd4e6a909f5f20624cd82957
Rdghostmann <randalchukzwilson@gmail.com>
2026-03-14 10:37:57 -0700

    Added updated

--- app/dashboard/transactions/page.jsx
+++ app/dashboard/transactions/page.jsx
@@ -6,10 +6,14 @@ import { authOptions } from "@/auth";
 
 export default async function TransactionsRoute() {
   const session = await getServerSession(authOptions);
+  const Uuser = session.user.id;
 
   if (!session?.user?.id) {
     redirect("/login");
   }
+
+  console.log(Uuser);
+  
 
   return <TransactionPage userId={session.user.id} />;
 }
